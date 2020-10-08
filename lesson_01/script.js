5c3613d6d7e7efd4618f3c13dd0ac004fbb0b21c
as-webagency <as.webagency2020@gmail.com>
2020-10-08 07:15:35 +0300

    add lesson_01

---
+++ lesson_01/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let money; // Деньги
+let income; // Доход 
+let addExpenses; // Добавить расходы
+let deposit; // Депозит
+let mission; // Миссия
+let period; // Период
+
+alert( 'Привет, мир' );
+console.log( 'Привет, мир' );
