8ef5b46d8889e0a8bebc55fc65dd5c35e2ffdb42
as-webagency <as.webagency2020@gmail.com>
2020-10-20 22:07:24 +0300

    add lesson_07_headr

--- lesson_07_hard/script.js
+++ lesson_07_hard/script.js
@@ -4,9 +4,27 @@ const week = ['Понедельник', 'Вторник', 'Среда', 'Чет�
 
 for ( let i = 0, len = week.length; i < len; i++ ) {
     let html = week[i];
-    if ( i === 0 ) {
-        html = html.italics();
-    } else if ( i > 4 ) {
+
+    if ( i === 4 ) {
+        switch (i) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                html = html.italics();
+                break;
+            case 5:
+            case 6:
+                html = html.bold();
+                html = html.italics();
+                break;
+        
+            default: 
+                alert( 'Что-то пошло не так' );
+        }
+    }
+    if ( i === 5 || i === 6 ) {
         html = html.bold();
     }
 
