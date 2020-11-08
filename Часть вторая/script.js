46f4ed5143cd3ae4a02a5fefbc1fa36cd9de7b03
as-webagency <as.webagency2020@gmail.com>
2020-11-08 14:37:23 +0200

    add lesson_14 - часть вторая

--- Часть вторая/script.js
+++ Часть вторая/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+class First {
+    hello () {
+        console.log( 'Привет я метод родителя!' );
+    }
+}
+class Second extends First {
+    hello () {
+        super.hello();
+        console.log( 'А я наследуемый метод!' );
+    }
+}
+
+const first = new First();
+const second = new Second();
+
+//first.hello();
+second.hello();
